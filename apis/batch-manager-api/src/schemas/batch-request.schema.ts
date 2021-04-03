080e006cf250c023b8d0a7ec7acead4183e37a74
Dheerendra.PANDEY-ext <Dheerendra.PANDEY-ext@aldautomotive.com>
2021-04-03 06:33:46 +0530

    Updated max limit.

--- apis/batch-manager-api/src/schemas/batch-request.schema.ts
+++ apis/batch-manager-api/src/schemas/batch-request.schema.ts
@@ -1,5 +1,5 @@
 const Joi = require('joi');
 export const batchRequestSchema = Joi.object().keys({
-    batchSize: Joi.number().integer().min(1).max(250).required(),
-    numbersPerBatch: Joi.number().integer().min(1).max(250).required(),
+    batchSize: Joi.number().integer().min(1).max(150).required(),
+    numbersPerBatch: Joi.number().integer().min(1).max(150).required(),
 });
